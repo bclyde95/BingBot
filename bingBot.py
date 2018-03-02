72a26fcded48244cc373007854207401390241ad
Brandon Clyde <bclyde9514@gmail.com>
2018-03-01 20:08:08 -0800

    Create a log_in function , change the implementation of the bot searches

--- bingBot.py
+++ bingBot.py
@@ -16,42 +16,101 @@ from twitterTerms import TweetTerms
 
 class BingBot:
     """A class to automate interactions with Bing. Includes login, semi-autonomous search (terms need to be input), and fully autonomous desktop and mobile search for rewards points"""
+
     def __init__(self, id):
-        """Constructor for BingBot. Initializes the url, id, and datalink"""
+        """Constructor for BingBot. Initializes the url, id, Datalink and TweetTerms classes, and generates the twitter search terms"""
+
         self._url = "http://bing.com"
         self._id = id
         self.data = DataLink()
-        twitter = TweetTerms()
-        self.terms = twitter.getHomeTweets()
+        self.twitter = TweetTerms()
+        self.terms = self.twitter.getHomeTweets()
 
     def _signed_in(self, browser):
         """Checks if signed in by checking if the 'sign in' text is displayed."""
+
         if(browser.find_element_by_id("id_s").is_displayed()):
             return False
         else:
             return True
 
-    def log_in(self, browser, mobile = False):
-        """Automates the sign in click and login screens"""
+    def _log_in(self, browser, mobile = False):
+        """The functionality of the public log_in() function"""
+
+        # Store the login data and the webpage elements
         login = self.data.getLogin(self._id)
+        desktop_signin = (By.ID, "id_l")
+        mobile_menu = (By.ID, "mHamburger")
+        mobile_signin = (By.CSS_SELECTOR, "#HBSignIn > a:nth-child(1)")
         email = (By.ID, "i0116")
         password = (By.ID, "i0118")
         button = (By.ID, "idSIButton9")
 
         if mobile:
-            WebDriverWait(browser,10).until(expected_conditions.element_to_be_clickable((By.ID, "mHamburger"))).click()
-            WebDriverWait(browser,10).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, "#HBSignIn > a:nth-child(1)"))).click()
+            # Expand mobile menu and click the sign in button
+            WebDriverWait(browser,10).until(expected_conditions.element_to_be_clickable(mobile_menu)).click()
+            WebDriverWait(browser,10).until(expected_conditions.element_to_be_clickable(mobile_signin)).click()
         else:
-            WebDriverWait(browser, 10).until(expected_conditions.element_to_be_clickable((By.ID, "id_l"))).click()
+            # Click the sign in button
+            WebDriverWait(browser, 10).until(expected_conditions.element_to_be_clickable(desktop_signin)).click()
 
+        # Fill email field and click the submit button
         WebDriverWait(browser, 10).until(expected_conditions.element_to_be_clickable(email)).send_keys(login["email"])
         WebDriverWait(browser, 10).until(expected_conditions.element_to_be_clickable(button)).click()
 
+        # Fill the password field and click the submit button
         WebDriverWait(browser, 10).until(expected_conditions.element_to_be_clickable(password)).send_keys(login["password"])
         WebDriverWait(browser, 10).until(expected_conditions.element_to_be_clickable(button)).click()
 
+    def log_in(self, mobile = False, headless=False):
+        """Automates the sign in click and login screens. 'mobile' mode when mobile=False"""
+
+        options = Options()
+        options.add_argument('-headless')
+        if (mobile):
+            profile = webdriver.FirefoxProfile()
+            profile.set_preference('general.useragent.override', "Apple iPhone 6s")
+            if (headless):
+                browser = webdriver.Firefox(profile, options=options)
+            else:
+                browser = webdriver.Firefox(profile)
+        else:
+            if (headless):
+                browser = webdriver.Firefox(options=options)
+            else:
+                browser = webdriver.Firefox()
+        if (mobile):
+            while(True):
+                try:
+                    browser.get(self._url)
+                    self._log_in(browser, mobile=True)
+                    break
+                except Exception:
+                    browser.quit()
+                    if (headless):
+                        browser = webdriver.Firefox(profile, options=options)
+                    else:
+                        browser = webdriver.Firefox(profile)
+                    continue
+        else:
+            while(True):
+                try:
+                    browser.get(self._url)
+                    if (self._signed_in(browser)):
+                        self._log_in(browser)
+                        break
+                except Exception:
+                    browser.quit()
+                    if (headless):
+                        browser = webdriver.Firefox(options=options)
+                    else:
+                        browser = webdriver.Firefox()
+                    continue
+        return browser
+
     def _find_term(self, browser):
         """Selects search terms from the links in the search results, pulling from a list if the terms to start and to avoid an endless loop"""
+
         try:
             time.sleep(0.5)
             links = browser.find_elements_by_xpath("//ol[@id='b_results']//li[@class='b_algo']//h2//a")
@@ -66,6 +125,7 @@ class BingBot:
 
     def search(self, browser, term):
         """Automates the search functionality, clearing the text box before each new search"""
+
         search_box = (By.ID, "sb_form_q")
         WebDriverWait(browser,10).until(expected_conditions.element_to_be_clickable(search_box)).send_keys(Keys.CONTROL + 'a', Keys.BACKSPACE)
         WebDriverWait(browser,10).until(expected_conditions.element_to_be_clickable(search_box)).send_keys(term, Keys.ENTER)
@@ -73,23 +133,12 @@ class BingBot:
     def desktop(self):
         # *** NEED TO TEST ***
         """The automation function for maxing out desktop points"""
-        options = Options()
-        options.add_argument('-headless')
-        browser = webdriver.Firefox(options=options)
-        while(True):
-            try:
-                browser.get(self._url)
-                if (self._signed_in(browser)):
-                        self.log_in(browser)
-                        break
-            except Exception:
-                browser.quit()
-                browser = webdriver.Firefox()
-                continue
+
+        browser = self.log_in(headless=True)
         term = ""
-        points = self.data.getPoints(self._id)
+        points = int(WebDriverWait(browser,10).until(expected_conditions.element_to_be_clickable((By.ID, "id_rc"))).text)
         pointGoal = points + 150
-        while(points < pointGoal):
+        while(points <= pointGoal):
             time.sleep(0.5)
             points = int(WebDriverWait(browser,10).until(expected_conditions.element_to_be_clickable((By.ID, "id_rc"))).text)
             while(True):
@@ -107,20 +156,8 @@ class BingBot:
     def mobile(self):
         # *** IN PROGRESS ***
         """The automation function for maxing out mobile points"""
-        profile = webdriver.FirefoxProfile()
-        profile.set_preference('general.useragent.override', "Apple iPhone 6s")
-        options = Options()
-        options.add_argument('-headless')
-        browser = webdriver.Firefox(profile, firefox_options=options)
-        while(True):
-            try:
-                browser.get(self._url)
-                self.log_in(browser, mobile=True)
-                break
-            except Exception:
-                browser.quit()
-                browser = webdriver.Firefox(profile, firefox_options=options)
-                continue
+        
+        browser = self.log_in(mobile=True, headless=True)
         term = ""
         i = 0
         while(i < 30):
